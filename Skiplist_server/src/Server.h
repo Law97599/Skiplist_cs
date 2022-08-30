209f50b7d29ec36ad56375768547cb083a942e75
JasonLaw <luozuxuan@foxmail.com>
2022-08-30 23:59:11 +0800

    format.

--- Skiplist_server/src/Server.h
+++ Skiplist_server/src/Server.h
@@ -32,33 +32,33 @@ class Server;
 class Client;
 // 对命令的函数指针进行typedef
 // 用命令的函数指针表示一个命令集合
-typedef void (*CommandFun)(Server *, Client *, string, string &, bool &);
+typedef void (*CommandFun)(Server*, Client*, string, string&, bool&);
 
 // 服务端
 class Server {
-  public:
-    int sListen;
-    sockaddr_in local;
-    //  端口号
-    int port;
-    // 服务端存储资源的数据库
-    // 数据库数组 每个元素是一个MyDB类型的指针
-    myDB *DB[dbNum];
-    // 命令集合
-    // 用一个map表示存储在服务端的命令集合
-    // key为命令的名字，value为一个指向函
-    // 数的指针，根据命令的名字指向对应的函数
-    map<string, CommandFun> Commands;
+ public:
+  int sListen;
+  sockaddr_in local;
+  //  端口号
+  int port;
+  // 服务端存储资源的数据库
+  // 数据库数组 每个元素是一个MyDB类型的指针
+  myDB* DB[dbNum];
+  // 命令集合
+  // 用一个map表示存储在服务端的命令集合
+  // key为命令的名字，value为一个指向函
+  // 数的指针，根据命令的名字指向对应的函数
+  map<string, CommandFun> Commands;
 };
 
 // 客户端
 class Client {
-  public:
-    Client(){};
-    int sClient;
-    sockaddr_in my_client;
-    // 客户端对应的数据库
-    myDB db;
+ public:
+  Client(){};
+  int sClient;
+  sockaddr_in my_client;
+  // 客户端对应的数据库
+  myDB db;
 };
 
 #endif
